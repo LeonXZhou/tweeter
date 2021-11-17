b274f6e4020f3b83f2143d63c55c69a7283f9c82
LeonXZhou <leon.zhou@queensu.ca>
2021-11-17 01:08:44 +0000

    refactored coded to use jquery instead of dom search

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
-  const tweetText = document.querySelector('#tweet-text');
-
+  /** Updates character counter based on the 140 - length of input string*/
   const updateCounter = function (event) {
-    const tweetLength = tweetText.value.length;
-    const counter = document.querySelector('.counter[name=counter][for=tweet-text]');
-    counter.innerHTML = 140 - tweetLength;
-    if (tweetLength > 140) {
-      counter.style.setProperty('color', 'red');
+    const counter = $('.counter[name=counter][for=tweet-text]');
+    const tweetText = $(this);
+
+    counter.text(140 - tweetText.val().length); // set counter to 140 minus the length of the input string
+    if ($('#tweet-text').val() > 140) { //controls color of tweet-text
+      counter.css('color', 'red');
     }
     else {
-      counter.style.setProperty('color', 'black');
+      counter.css('color', 'black');
     }
   }
 
-  tweetText.addEventListener('input', updateCounter);
+  $('#tweet-text').on('input',null,updateCounter);
 });
