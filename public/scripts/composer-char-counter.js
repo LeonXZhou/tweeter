2e9b2a8fe27590dee45fa14796fe02ee6702f135
LeonXZhou <leon.zhou@queensu.ca>
2021-11-17 00:19:32 +0000

    implemented counter animation

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,17 @@
-$(document).ready(function() {
+$(document).ready(function () {
   const tweetText = document.querySelector('#tweet-text');
-  const updateCounter = function(event){
-    console.log(tweetText.value);
+
+  const updateCounter = function (event) {
+    const tweetLength = tweetText.value.length;
+    const counter = document.querySelector('.counter[name=counter][for=tweet-text]');
+    counter.innerHTML = 140 - tweetLength;
+    if (tweetLength > 140) {
+      counter.style.setProperty('color', 'red');
+    }
+    else {
+      counter.style.setProperty('color', 'black');
+    }
   }
 
-  tweetText.addEventListener('input',updateCounter);
+  tweetText.addEventListener('input', updateCounter);
 });
