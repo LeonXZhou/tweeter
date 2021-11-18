459762d1840f7a0841f400818347c9a74d81c639
LeonXZhou <leon.zhou@queensu.ca>
2021-11-18 00:06:24 +0000

    add $ to jquery variable names in compser-char-counter.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
   $.ajax({ url: '/tweets/', method: 'GET', })
     .then((results) => {
-      console.log(results)
     })
     .catch((error) => {
       console.log('error:', error);
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
   /** Updates character counter based on the 140 - length of input string*/
   const updateCounter = function (event) {
-    const counter = $('.counter[name=counter][for=tweet-text]');
-    const tweetText = $(this);
+    const $counter = $('.counter[name=counter][for=tweet-text]');
+    const $tweetText = $(this);
 
-    counter.text(140 - tweetText.val().length); // set counter to 140 minus the length of the input string
+    $counter.text(140 - $tweetText.val().length); // set counter to 140 minus the length of the input string
     if ($('#tweet-text').val() > 140) { //controls color of tweet-text
-      counter.css('color', 'red');
+      $counter.css('color', 'red');
     }
     else {
-      counter.css('color', 'black');
+      $counter.css('color', 'black');
     }
   }
 
